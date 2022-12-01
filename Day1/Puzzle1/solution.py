277826c8f27c583907a4939c3dc330b02f821e68
sobyandrew <sobyandrew@gmail.com>
2022-12-01 09:04:15 -0800

    day1 puzzle 1/2

---
+++ Day1/Puzzle1/solution.py
@@ -0,0 +1,26 @@
+#day 1 solution 1
+
+with open('input.txt') as f:
+    lines = f.readlines()
+
+elfTotals = []
+currElf = 0
+
+for line in lines:
+    if line.strip() == "":
+        elfTotals.append(currElf)
+        currElf = 0
+    else:
+        currElf += int(line.strip())
+elfTotals.append(currElf)
+print(max(elfTotals))
+
+top3 = 0
+
+for i in range(0,3):
+    currMax = max(elfTotals)
+    top3 += currMax
+    elfTotals.remove(currMax)
+
+print(top3)
+
